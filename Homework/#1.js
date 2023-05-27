aa35030e4e8ab991a2f659ba07cb6039b41b51e4
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-05-27 16:15:46 +0300

    Homework 1 Completed

---
+++ Homework/#1.js
@@ -0,0 +1,11 @@
+const arguments = process.argv.slice(2);
+
+function circleAreaCalc(radius) {
+  const pi = 3.14159265359;
+  const area = pi * Math.pow(radius, 2);
+  console.log(
+    `Yarıçapı (${radius}) olan dairenin alanı: (${parseFloat(area).toFixed(2)})`
+  );
+}
+
+CircleAreaCalc(arguments[0]);
