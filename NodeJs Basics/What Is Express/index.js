2646bd42861cf0ff492fefdc9a6c0314f20d11a9
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-05-31 17:18:42 +0300

    What Is Express

---
+++ NodeJs Basics/What Is Express/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.status(200).send('Hello World')
+})
+
+app.get('/about', (req,res) =>{
+    res.status(200).send('About Sayfası')
+})
+
+app.get('/contact', (req,res) =>{
+    res.status(200).send('Contact Sayfası')
+})
+
+app.get('*', (req,res) =>{
+    res.status(404).send('404 Sayfa Bulunamadı')
+})
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Sunucu port ${port} da çalışmaya başladı...`)
+})
