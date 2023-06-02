3b58a65a37eb1ee2d40809e3b2cb8b62c85ee9d4
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-02 20:29:38 +0300

    Static Files Uploaded

--- Projects/PCAT/app.js
+++ Projects/PCAT/app.js
@@ -1,19 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//Middlewares
+app.use(express.static('public'));
+
 const port = 3000;
 
-app.get('/', (req,res) =>{
-    
-    const photo = {
-        id:1,
-        name: "Photo Name",
-        description: "Photo Description"
-    } 
-    res.send(photo)
-    
-})
-
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
