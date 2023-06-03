708be89ddc556e4d6fcea2f875e753ee02bf1f60
Cengiz ERYİĞİT <cengiz_eryigit@hotmail.com>
2023-06-03 14:37:56 +0300

    Template Engine Concept

--- Projects/PCAT/app.js
+++ Projects/PCAT/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+//Template Engine
+app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
@@ -9,7 +12,13 @@ app.use(express.static('public'));
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
